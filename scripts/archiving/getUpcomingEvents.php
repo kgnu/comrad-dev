f748f8040b34ff988eb48c1c005e5b5273b0c659
seankwilliams <seankwilliams@yahoo.com>
2016-02-16 21:08:57 -0800

    Get Upcoming Events change (precache more days) and giveaway email copy changes

--- scripts/archiving/getUpcomingEvents.php
+++ scripts/archiving/getUpcomingEvents.php
@@ -1,44 +1,71 @@
 <?php
 
 	//created sean williams 4/4/11
-	//to be run every 30 minutes
+	//to be run every hour
 	//reads upcoming shows from the database and writes them out to a text file
 	//writes info of shows to be recorded into a text file
 	
+	//set a script timeout of thirty minutes
+	set_time_limit(30 * 60);
+	
+	//include the configuration and log file
+	require('config.php');
+	require('log.php'); 
+	
+	//this error handler will write out any PHP errors
 	set_error_handler('errorHandler');
 	function errorHandler($errno, $errstr, $errfile, $errline) {
-		echo '<br /><b>Error</b>: ' . $errstr . ' (line '.$errline.' in '.$errfile.')<br />';
+		logText('PHP Error from getUpcomingEvents.php: ' . $errstr . ' (line '.$errline.' in '.$errfile.')');
 	}
 	
-	$root = "../../../"; //the path to the root of the comrad-dev folder
+	logText('Started executing getUpcomingEvents.php');
 	
-	//initialize classes
-	require_once($root.'lib/classes/Initialize.php');
-	$init = new Initialize();
-	$init->setAutoload();
-	
-	//get all events in the next day
-	$em = EventManager::getInstance();
-	$events = $em->getEventsBetween(time(), time() + 60*60*24);
+	//sw changed file on 8/16/12 to pull from geteventsbetween.php API instead of data access layer
+	$apiUrl = 'http://kgnu.org/playlist/ajax/geteventsbetween.php?start=' . time() . '&end=' . (time() + 60*60*24*3); 
+	logText('accesing url at ' . $apiUrl);
+	$contents = file_get_contents($apiUrl);
+	$events = json_decode($contents, TRUE);		
 	
 	//loop through the next day's events, and prepare a string to store in a text file
 	$upcomingEvents = "";
-	$startTime = time(); //TODO: make these real values from the database
-	$endTime = time() + 400; //TODO: make these real values from the database
+	if (count($events) == 0) {
+		logText('ERROR: no events found for upcoming events. Exiting getUpcomingEvents script.');
+		exit();
+	}
+	
+	//sort events 
+	usort($events, 'sortByStartDateTime');
+	
 	foreach ($events as $scheduledEventInstance) {
-		$scheduledEvent = $scheduledEventInstance->ScheduledEvent;
-		$event = $scheduledEvent->Event;
-		//if we're set up to record
-		if ($event->RecordAudio) { //TODO: verify that this is a correct check
-			$upcomingEvents .= $event->Title . "|" . $startTime  . "|" . $endTime . "\n";
-			$startTime = $startTime + 460; //TODO: make these real values from the database
-			$endTime = $endTime + 460; //TODO: make these real values from the database
+		$scheduledEvent = $scheduledEventInstance['Attributes']['ScheduledEvent'];
+		$event = $scheduledEvent['Attributes']['Event']['Attributes'];
+		//if we're supposed to record the event, store the event in the text file
+		if (isset($event['RecordAudio']) && $event['RecordAudio']) {
+			$title = preg_replace("/[^A-Za-z0-9]/","",$event['Title']); //strip all non-alphanumeric characters out of the title, since this will be used for a file/folder name
+			$startTime = $scheduledEventInstance['Attributes']['StartDateTime'];
+			$recordingStartTime = $startTime + ($scheduledEvent['Attributes']['RecordingOffset'] * 60);
+			$endTime = $startTime + ($scheduledEventInstance['Attributes']['Duration'] * 60);
+			$showType = $event['Category'];
+			$upcomingEvents .= $title . "|" . $recordingStartTime  . "|" . $endTime .  "|" . $startTime . "|" . $event['Title'] . "|" . 
+							 (isset($event['Host']) ? $event['Host']['Attributes']['Name'] : '') 
+							 . "|" . $event['Id'] . "|" . 
+							 (isset($event['HostId']) ? $event['HostId'] : '') . 
+							 "|".$showType."\n";
 		}
 	}
 	
+	
 	//write out the upcoming events to a file
-	$upcomingEventsFile = "/var/www/comrad-dev-read-only/lib/utilities/archiving/upcomingEvents.txt"; //if updating this, also update the same variable in archiveShows.php
 	$fh = fopen($upcomingEventsFile,'w');
 	fwrite($fh,$upcomingEvents);
 	fclose($fh);
+	
+	logText('Finished executing getUpcomingEvents.php');
+	
+	truncateLog();
+
+	
+	function sortByStartDateTime($a, $b) {
+		return ($a['Attributes']['StartDateTime'] > $b['Attributes']['StartDateTime']);
+	}
 ?>
